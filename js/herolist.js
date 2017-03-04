0f2ce2c970235588c9cfe93835aaf183dec4e29f
yuanchunze1978 <yuanchunze@qq.com>
2017-03-04 16:02:43 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -1,7 +1,3 @@
 function loadJson(){
-  var txt1="<p>Text.</p>";               // 以 HTML 创建新元素
-var txt2=$("<p></p>").text("Text.");   // 以 jQuery 创建新元素
-var txt3=document.createElement("p");  // 以 DOM 创建新元素
-txt3.innerHTML="Text.";
-$("p").append(txt1,txt2,txt3);         // 追加新
+ "<h1 >英雄榜12345</h1>"
 }
