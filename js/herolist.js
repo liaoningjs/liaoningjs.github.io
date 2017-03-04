e8a9b50639d98a09be0a33ed39465c12b5087399
yuanchunze1978 <yuanchunze@qq.com>
2017-03-04 15:01:49 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -1,10 +1,3 @@
 function loadJson(){
-  var parent = document.getElementById("herolist");
-　　　　//添加 div
-　　　　var div = document.createElement("h1");
-　　　　//设置 div 属性，如 id
-　　　　//div.setAttribute("id", "newDiv");
- 
-　　　　div.innerHTML = "js 动态添加div";
-　　　　parent.appendChild(div);
+  alert("111");
 }
