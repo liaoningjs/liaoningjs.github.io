9c4362165df4192e38c5c15177962db14f2e73f5
yuanchunze1978 <yuanchunze@qq.com>
2017-03-04 16:18:01 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -1,3 +1,12 @@
 function loadJson(){
 
 }
+ function getData()
+    {
+        return ["oec2003", 25]
+    }
+    function getInfo()
+    {
+        var info = getData();
+        alert("姓名：" + info[0] + "年龄：" + info[1]);
+    }
