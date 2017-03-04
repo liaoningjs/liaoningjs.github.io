74977d8a61c5c99bdba48df72b50eb7315f92e53
yuanchunze1978 <yuanchunze@qq.com>
2017-03-04 15:06:02 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -1,3 +1,7 @@
 function loadJson(){
-  alert("111");
+  var txt1="<p>Text.</p>";               // 以 HTML 创建新元素
+var txt2=$("<p></p>").text("Text.");   // 以 jQuery 创建新元素
+var txt3=document.createElement("p");  // 以 DOM 创建新元素
+txt3.innerHTML="Text.";
+$("p").append(txt1,txt2,txt3);         // 追加新
 }
