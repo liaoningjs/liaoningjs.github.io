bec898f7f340495ebd216461802de1ff14d7d997
yuanchunze1978 <yuanchunze@qq.com>
2017-03-05 16:53:52 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -9,16 +9,14 @@
 
 function loadJson(){
     document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
-    var areadata ={ 
-    province : [] 
-   
-    } 
-   function initAreaData(){ 
-   var dataroot="/herolist/liaoning.json"; 
-   $.getJSON(dataroot, function(data){ 
-   areadata.province=data.province; 
+    $("button").click(function(){
+  $.getJSON("/herolist/liaoningjs.json",function(result){
+    $.each(result, function(i, field){
+      $("div").append(field + " ");
+    });
+  });
+});
     
 }); 
 
 }
- 
