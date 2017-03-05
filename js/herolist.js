542c87ec253092a08eb89ff88e08ee5022e24b7d
yuanchunze1978 <yuanchunze@qq.com>
2017-03-05 17:01:41 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -9,6 +9,8 @@
 
 function loadJson(){
     document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
-   
+    $.getJSON("http://liaoningjs.com/herolist/liaoningjs.json",function(data){
+    //data就是json对象了，不需要在转换
+});   
 
 }
