d9ded42f5492218563b015c45d8e982ecae254ad
yuanchunze1978 <yuanchunze@qq.com>
2017-03-05 16:17:38 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -15,13 +15,16 @@ function loadJson(){
       ts = fso.OpenTextFile("/herolist/liaoningjs.json", ForReading);
       s = ts.ReadLine();
       var json = eval('(' + s + ')');
-      alert(json.MyData[0].id);
+      //alert(json.MyData[0].id);
+      console.log(json.MyData[0].id);
     }
     catch(err){
+        console.log("err");
         throw err;
  
     }
     finally{
+        console.log("wancheng");
      ts.Close();
      }
 }
