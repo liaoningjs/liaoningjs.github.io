a41cedf25c1c793b4f03736f8fd79c9bd54eb187
yuanchunze1978 <yuanchunze@qq.com>
2017-03-05 17:07:38 +0800

    no message

--- js/herolist.js
+++ js/herolist.js
@@ -8,9 +8,12 @@
 
 
 function loadJson(){
+    var str;
     document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
     $.getJSON("http://liaoningjs.com/herolist/liaoningjs.json",function(data){
-    document.getElementById("demo").innerHTML=data;
+    $.each(data,function(i,value){  
+            str+=value;     
+        });      
 });   
-
+document.getElementById("demo").innerHTML=str;
 }
